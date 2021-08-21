d4e77fb6b738172193f4e83b69c400a982078c0a
wzwz5200 <80612049+wzwz5200@users.noreply.github.com>
2021-08-21 19:58:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ cat << EOF > ${DIR_TMP}/heroku.json
     }]
 }
 EOF
-
+///////12321312321333333333333333
 # Get V2Ray executable release
 curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
 busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
